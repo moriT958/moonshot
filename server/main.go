abc5d5da1474f35530ae63daace301e99d68c7b3
morita <morit958@gmail.com>
2026-02-08 16:41:19 +0900

    refactor3

--- server/main.go
+++ server/main.go
@@ -13,12 +13,6 @@ func main() {
 		ServeWs(room, w, r)
 	})
 
-	// Serve static files
-	http.Handle("/", http.FileServer(http.Dir("../web")))
-
-	// Serve the built JS file
-	http.Handle("/_build/", http.StripPrefix("/_build/", http.FileServer(http.Dir("../_build"))))
-
 	log.Println("Server starting on :8080")
 	log.Println("Open http://localhost:8080 in your browser")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
